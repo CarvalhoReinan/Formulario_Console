0516b177531de3e80053f3bdfa832f217e754693
Reinan Carvalho <reinancdj55@gmail.com>
2025-06-09 17:50:56 -0300

    listas

---
+++ config.py
@@ -0,0 +1,32 @@
+# config.py
+
+frameworks = [
+    "CherryPy",
+    "Bottle",
+    "Flask",
+    "FastAPI",
+    "Django",
+    "Outro?",
+    "Cancelar"
+]
+
+horas = [
+    "0-30 Min",
+    "1-2 Horas",
+    "3-4 Horas",
+    "Outro?"
+]
+
+estudo = [
+    "Youtube",
+    "Google",
+    "Livro",
+    "Outro?"
+]
+
+nivel = [
+    "Iniciante",
+    "Intermediário",
+    "Avançado",
+    "Cancelar"
+]
