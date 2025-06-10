4481a72d364f1fa6c2ea19e918e3d75890a3bb50
Reinan Carvalho <reinancdj55@gmail.com>
2025-06-10 10:02:33 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from formulario-console import perguntar_tempo
+from formulario_Console import perguntar_tempo
+import emoji
+import os
 
-def main():
-    nome = input("Qual é o seu nome? ").capitalize()
+def boas_vindas():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(emoji.emojize("👋 Olá! Bem-vindo ao seu assistente de estudos em Python 🐍", language='pt'))
+
+def iniciar_programa():
+    boas_vindas()
+    nome = input("Qual é o seu nome? ")
     perguntar_tempo(nome)
 
 if __name__ == "__main__":
-    main()
-
+    iniciar_programa()
